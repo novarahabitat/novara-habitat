e8b4bd66fea1f193f9735cc9ab3e42fcbb78812e
novarahabitat <vitallaurent@googlemail.com>
2026-05-23 18:37:38 +0200

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,3 +0,0 @@
-import type { Config } from 'tailwindcss'
-const config: Config = { content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'], theme: { extend: { colors: { novara: { black: '#070706', charcoal: '#11100d', gold: '#c9a45c', soft: '#f4efe5' } } } }, plugins: [] }
-export default config
