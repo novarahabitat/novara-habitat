a6379924b5cc0a563415df78c5210e36ecee05e9
novarahabitat <vitallaurent@googlemail.com>
2026-06-07 18:22:07 +0200

    Create SafetyIncidentButton.tsx

---
+++ components/core/SafetyIncidentButton.tsx
@@ -0,0 +1,8 @@
+import SafetyIncidentButton from "@/components/core/SafetyIncidentButton";
+
+export default function CorePage() {
+  return (
+    <main className="min-h-screen bg-[#fbf7ff] text-[#34275f]">
+      <SafetyIncidentButton />
+
+      <section className="mx-auto flex min-h-screen max-w-md flex-col px-5 pb-28 pt-6 md:max-w-4xl">
